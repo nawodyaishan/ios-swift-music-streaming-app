870c177287784dfa8483ca73cb2b7b9115fbb764
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-04 15:22:55 +0530

    Blur

---
+++ UrMusicApp/BlurEffect.swift
@@ -0,0 +1,16 @@
+//
+//  BlurEffect.swift
+//  UrMusicApp
+//
+//  Created by Nawodya Ishan on 2022-03-04.
+//
+import SwiftUI
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
