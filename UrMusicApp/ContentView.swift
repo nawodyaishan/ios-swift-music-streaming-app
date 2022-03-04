4ac75f7cee8816471543900c270b4c8e06ce6a66
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-04 15:20:21 +0530

    Update

--- UrMusicApp/ContentView.swift
+++ UrMusicApp/ContentView.swift
@@ -57,10 +57,11 @@ struct ContentView : View {
         NavigationView{
             ScrollView{
                 ScrollView(.horizontal, showsIndicators: false, content: {
+                    LazyHStack{
                     ForEach(self.albums, id: \.self, content: {
                         album in
                         AlbumArt(album: album)
-                    })
+                    })}
                 })
                 
                 LazyVStack{
@@ -70,6 +71,7 @@ struct ContentView : View {
                                                                                        Song(name: "Song 4", time : "2:35"),
                                                                                        Song(name: "Song 5", time : "2:35")], id: \.self, content: {
                         song in
+                        SongCell(song: song)
                         
                     })
                 }
@@ -82,6 +84,12 @@ struct AlbumArt : View {
     var album : Album
     var body: some View{
         EmptyView()
+        ZStack {
+            Image(album.image).resizable().aspectRatio(contentMode: .fill).frame(width: 170, height: 170, alignment: .center)
+            ZStack{
+                Text(album.name).foregroundColor(.white)
+            }
+        }.clipped().cornerRadius(20).shadow(radius: 10).padding(15)
     }
 }
 
@@ -95,6 +103,10 @@ struct SongCell : View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AlbumArt(album: Album(name: "Album 1", image: "1", songs: [Song(name: "Song 1", time : "2:35"),
+                                                                         Song(name: "Song 2", time : "2:35"),
+                                                                         Song(name: "Song 3", time : "2:35"),
+                                                                         Song(name: "Song 4", time : "2:35"),
+                                                                         Song(name: "Song 5", time : "2:35")]))
     }
 }
