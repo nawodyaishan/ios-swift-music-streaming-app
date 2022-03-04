daa617a58cb21ece99ead2b457b3ab1871d5b84b
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-04 09:24:22 +0530

    Update

---
+++ UrMusicApp/UrMusicApp/UrMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrMusicAppApp.swift
+//  UrMusicApp
+//
+//  Created by Nawodya Ishan on 2022-03-04.
+//
+
+import SwiftUI
+
+@main
+struct UrMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
